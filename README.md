dcdd60abd9bb1c034a27ccb999b4272423c82893
weihsun.weng <weihsun@pmi-ai.com>
2021-02-04 18:03:46 +0800

    Add economic api

---
+++ README.md
@@ -0,0 +1,11 @@
+config.yml
+```yaml
+server:
+  port: 8079
+mongodb:
+  host: {host}:{port}
+  database: {database}
+  username: {username}
+  password: {password}
+  authentication-database: {authSource}
+```
--- config/mongodb.go
+++ config/mongodb.go
@@ -12,15 +12,22 @@ type MongoDB struct {
 
 }
 
-func NewMongoClient(config *Config) (*mongo.Database, error) {
+func NewMongoDatabase(config *Config) (*mongo.Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	credential := options.Credential{
+		Username: config.Mongodb.UserName,
+		Password: config.Mongodb.Password,
+		AuthSource: config.Mongodb.AuthenticationDatabase,
+	}
+	mongoOptions := options.Client().SetHosts([]string{config.Mongodb.Host}).SetAuth(credential)
+
 	defer cancel()
-	//TODO
-	client, err := mongo.Connect(ctx, options.Client().SetHosts([]string{config.Mongodb.Host}))
+	client, err := mongo.Connect(ctx, mongoOptions)
 	if err != nil{
 		log.Printf("Connect Database err #%v", err)
 		return nil, err
 	}
-	return client.Database("economic_dev"), nil
+	return client.Database(config.Mongodb.Database), nil
 }
 
